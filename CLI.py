6bf4ee3bc014dec0a211e93255a842d97f1c4abb
Aliens <flaktor@blackholeatthecenterofthemilkyway.com>
2025-05-10 10:49:05 -0500

    Update CLI.py

---
+++ CLI.py
@@ -0,0 +1,8 @@
+print ("\x1B[H\x1B[J" +\
+       "This is a placeholder for the actual code that will be put here.\n" +\
+       "If you want to take a look at the VERY WIP code, go to the testing branch.\n" +\
+       "You rock!\n")
+
+input("Press ENTER to close")
+
+print ("\x1B[H\x1B[J")
